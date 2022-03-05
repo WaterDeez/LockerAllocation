77fd39f2f5ab42dd2b580081c8566491b6d50351
WaterDeez <water997@pm.me>
2022-03-05 20:54:25 +1000

    New Lock Finalised

--- LockerAllocation/MainForms/NewLock.vb
+++ LockerAllocation/MainForms/NewLock.vb
@@ -5,23 +5,20 @@
         ComboBox4.Items.Clear()
 
         ' RUN QUERY
-        SQL.ExecQuery("SELECT DISTINCT * FROM StudentDetails ORDER BY Surname")
+        SQL.ExecQuery("SELECT DISTINCT *, (Surname + ', ' + PreferredNames) AS StudentName FROM StudentDetails ORDER BY Surname")
 
         If Sql.HasException(True) Then Exit Sub
 
-        ' LOOP ROW & ADD TO COMBOBOX
-        For Each r As DataRow In Sql.DBDT.Rows
-            ComboBox4.Items.Add(r("Surname").ToString & ", " & r("PreferredNames").ToString)
-        Next
+        ComboBox4.DataSource = SQL.DBDT
+        ComboBox4.DisplayMember = "StudentName"
+        ComboBox4.ValueMember = "StudentID"
     End Sub
     Private Sub InsertLock()
         Dim combination As String = ComboBox1.Text + "." + ComboBox2.Text + "." + ComboBox3.Text
-        SQL.AddParam("@combination", combination)
-        SQL.AddParam("@serialnunber", TextBox1.Text)
         If CheckBox1.Checked = True Then
-            SQL.ExecQuery("INSERT INTO Locks (SerialNumber,Combination,Active,StudentID,DateAdded) VALUES (@serialnumber,@combination,1," + ComboBox4.Text + ",GETDATE());")
+            SQL.ExecQuery("INSERT INTO Locks VALUES ('" + TextBox1.Text + "','" + ComboBox1.Text + "." + ComboBox2.Text + "." + ComboBox3.Text + "',1,'" + ComboBox4.SelectedValue.ToString + "',GETDATE());")
         Else
-            SQL.ExecQuery("INSERT INTO Locks (SerialNumber,Combination,Active,DateAdded) VALUES (@serialnumber,@combination,0,GETDATE());")
+            SQL.ExecQuery("INSERT INTO Locks (SerialNumber,Combination,Active,DateAdded) VALUES ('" + TextBox1.Text + "','" + ComboBox1.Text + "." + ComboBox2.Text + "." + ComboBox3.Text + "',0,GETDATE());")
         End If
         'Report & Abort on SQL Errors
         If SQL.HasException(True) Then Exit Sub
@@ -34,10 +31,17 @@
         ComboBox2.SelectedIndex = "0"
         ComboBox3.SelectedIndex = "0"
         ComboBox4.SelectedItem = Nothing
-        LoadStudents()
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        InsertLock()
+        If TextBox1.Text = "" Then
+            MsgBox("Enter a serial number")
+        Else
+            If ComboBox1.SelectedIndex = 0 AndAlso ComboBox2.SelectedIndex = 0 AndAlso ComboBox3.SelectedIndex = 0 Then
+                MsgBox("Enter a valid combination")
+            Else
+                InsertLock()
+            End If
+        End If
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -46,6 +50,7 @@
 
     Private Sub NewLock_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ClearDetails()
+        LoadStudents()
     End Sub
 
     Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
